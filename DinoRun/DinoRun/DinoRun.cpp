09af22c0dec7f9487d3152dbafe6429e40f55553
sobni <sobin1026@naver.com>
2024-05-31 14:39:03 +0900

    gravity

--- DinoRun/DinoRun/DinoRun.cpp
+++ DinoRun/DinoRun/DinoRun.cpp
@@ -22,7 +22,7 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 {
 	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) 
 	{
-		speed = 0.01f; gravity = 0.000981f;  a = 1;
+		speed = 0.01f; gravity = 0.00098f;  a = 1;
 		//  100        9.8
 		// .0100     .00098
 	}
@@ -101,7 +101,7 @@ int main(void)
 		{
 			case 1:
 				j += speed;
-				if (j>= 0.3f) { a = 2; }
+				if (j>= 0.9f) { a = 2; }
 				break;
 			case 2:
 				speed += gravity;
